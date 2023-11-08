8685715b72c874f72d41f318b0eec5a2ba2c80e4
Natalia Andrea Lossada <natilossada@hotmail.com>
2023-11-08 10:27:35 -0300

    Homework clase ES6 terminada

--- FT-M2-master/02-ES6/homework/src/selector.js
+++ FT-M2-master/02-ES6/homework/src/selector.js
@@ -1,17 +1,31 @@
 var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
+  if (typeof startEl === "undefined") {  // la primera vez toma valor de body, luego en recursión no entra acá ya tiene valor
+    startEl = document.body;              // en recursión pasa CADA ELEMENTO HIJO
   }
-
+  if (matchFunc(startEl)) resultSet.push(startEl)  //si no encuentra idoclass no pushea nada
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  let primerNivelArr = document.body.children
-  for (let i = 0; i < primerNivelArr; i++)
-
+  for (const child of startEl.children) { //recorrer array de hijos body
+    resultSet.push(...traverseDomAndCollectElements(matchFunc, child))
+    //con ... da cada hijos del child y lo pushea al array y aplico recursión con funcion match y CADA ELEMENTO HIJO(por loop del array)
+  }
+  return resultSet;
+  /*
+  Pseudo cód:
+  document.body.id ?? if si tiene id pero con qué comparo
+  si no --> bajar y buscar en los hijos y hacer un for o un map viendo si tiene el id cada elemento
+  y en cada elemento aplicar recursividad
+      let primerNivelArr = document.body.children
+      for (let i = 0; i < primerNivelArr.length; i++) {
+        if verificar aca en cada elemento si coincide, no se como
+      }
+      if (primerNivelArr.length>0)
+  
+      */
 };
 
 // Detecta y devuelve el tipo de selector
@@ -30,50 +44,34 @@ var selectorTypeMatcher = function (selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-
-//DE ESTA FUNCIÓN NO TOCO NADA????
-// var matchFunctionMaker = function (selector) {
-//   var selectorType = selectorTypeMatcher(selector);
-//   var matchFunction;
-//   if (selectorType === "id") {
-//     matchFunction = function (element) {
-//       return element.id === selector.slice(1)
-//     }
-//     // element.getAttribute("id") === selector.slice(1)
-//   } else if (selectorType === "class") {
-//     matchFunction = function (element) {
-//       return element.className === selector.slice(1)
-//     }
-//   } else if (selectorType === "tag.class") {
-//     matchFunction = function (element) {
-//       var [tag, classOfTag] = selector.split('.');
-//       return element.tagName === tag && element.className === classOfTag
-//     }
-//   } else if (selectorType === "tag") {
-//     matchFunction = function (element) {
-//       element === selector
-//     }
-//   }
-
-//   return matchFunction;
-// };
-
 var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
-    matchFunction = function (element) {
+    matchFunction = (element) => {
       return element.id === selector.slice(1);
+      // `#${element.id}` === selector - otra opcion que agrega el hashtag al elemento para comparar
     }
   } else if (selectorType === "class") {
     matchFunction = function (element) {
       return element.classList.contains(selector.slice(1));
+      /*
+      for (let i=0; i< element.classList.length;i++) {
+          if(`.${element.classList[i]} === selector`) return true
+      }
+      return false
+      */
     }
 
   } else if (selectorType === "tag.class") {
-    var [tag, className] = selector.split('.');
+
     matchFunction = function (element) {
-      return element.tagName.toLowerCase() === tag && element.classList.contains(className);
+      // return element.tagName.toLowerCase() === tag && element.classList.contains(className);
+      // }
+      var [tag, className] = selector.split('.'); //destructuring del array
+      let tagFunc = matchFunctionMaker(tag); // le paso la misma función con tag como selector
+      let ClassFunc = matchFunctionMaker(`.${className}`); // le paso misma función con selector clase con el punto
+      return tagFunc(element) && ClassFunc(element) // paso la var de la funcion con argumento devuelve true si ambos son true
     }
   } else if (selectorType === "tag") {
     matchFunction = function (element) {
