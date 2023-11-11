b505c7ba1afb5f8b47e86c2870f93fc4f5398e19
Natalia Andrea Lossada <natilossada@hotmail.com>
2023-11-11 12:19:50 -0300

    Ejercicio 01 de react intro terminado

--- FT-M2-master/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2-master/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Natalia";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Welcome to ReactWorld</h1>
+      <h2>Our students and skills</h2>
+      <h3>{studentName}</h3>
+      <ul>
+        {
+          techSkills.map((skill) => {
+            return (
+              <li>{skill}</li>
+            )
+          })
+        }
+      </ul>
+      <Botones alerts={alerts}></Botones>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
